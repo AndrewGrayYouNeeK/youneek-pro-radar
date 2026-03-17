aa866ef2d31669e76a806c3f0c863a32a3c5cb3b
Andrew Gray <youneekartifacts@gmail.com>
2026-03-17 03:03:16 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-...
+import PageNotFound from './lib/PageNotFound';
+import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 // Add page imports here
 import RadarScope from "./pages/RadarScope";
 import Contacts from "./pages/Contacts";
